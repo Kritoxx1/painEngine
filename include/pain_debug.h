b8468eff8f2dc48f5e719616be07ec80ef3612b9
Kritoxx1 <turanturi08@gmail.com>
2026-05-02 14:29:12 +0200

    feat: add function attribute macros
    Note: The MSVC macros were added but not tested

--- include/pain_debug.h
+++ include/pain_debug.h
@@ -3,14 +3,32 @@
 
 #include <iostream>
 
-#define ensure(result, msg) \
-  if ((result) != VK_SUCCESS) \
-    throw std::runtime_error((msg));
+#if defined(_MSC_VER)
+# define PAIN_ALWAYS_INLINE_ATTR  __forceinline
+# define PAIN_NOINLINE_ATTR       __declspec(noinline)
+# define PAIN_NORETURN_ATTR       __declspec(noreturn)
+# define PAIN_COLD_ATTR           __attribute__((__cold__))
+
+#elif defined(__GNUC__) || defined(__clang__)
+# define PAIN_ALWAYS_INLINE_ATTR  __attribute__((__always_inline)) inline
+# define PAIN_NOINLINE_ATTR       __attribute__((__noinline__))
+# define PAIN_NORETURN_ATTR       __attribute__((__noreturn__))
+
+# define PAIN_CONST_ATTR          __attribute__((__const__))
+# define PAIN_COLD_ATTR           __attribute__((__cold__))
+# define PAIN_MALLOC_ATTR         __attribute__((__malloc__))
+
+# define nothing
+#endif
 
 #define __PAIN_LINE __LINE__
 #define __PAIN_FILE __FILE__
 
-inline void __attribute__((__noreturn__, __cold__))
+#define ensure(result, msg) \
+  if ((result) != VK_SUCCESS) \
+    throw std::runtime_error((msg));
+
+inline void PAIN_NORETURN_ATTR PAIN_COLD_ATTR
 _pain_assert_fail(const char* _msg, const char* _file, int _line) {
   std::cerr << "PAIN::ASSERTION::ERROR:: " << _msg << " at " << _file << ":" << _line;
   std::abort();
